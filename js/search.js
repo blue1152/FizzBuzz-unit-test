ellenlee-ac/movie-list-unit-test

movie list for search unit test

js/search.js
@@ -1,0 +1,4 @@
+function searchTitle (dataList, str) {
+  const regex = RegExp(str, 'i')
+  return dataList.filter(item => item.title.match(regex))
+}

blue1152/FizzBuzz-unit-test

js/search.js
@@ -1,0 +1,4 @@
+function searchTitle (data, str) {
+  const regex = RegExp(str, 'i')
+  return data.filter(item => item.title.match(regex))
+}